15e52353464fb73bef5e2c5034de32eca9103cd3
minsim1 <mindaugassimukaitis@gmail.com>
2025-09-03 21:16:21 +0300

    Added button and text

--- app/src/main/java/com/example/mipt_lab_2/MainActivity.java
+++ app/src/main/java/com/example/mipt_lab_2/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.mipt_lab_2;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,12 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button myButton = findViewById(R.id.mainButton);
+        TextView myTextView = findViewById(R.id.mainTextView);
+
+        myButton.setOnClickListener(v ->
+                myTextView.setText(getString(R.string.textOnButtonClick))
+        );
     }
 }
